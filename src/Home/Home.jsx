d9b25168538f0540df189f347a4c5770be803836
jtojanci <jtojanci@gmail.com>
2024-04-04 16:24:38 -0500

    se desarrolla el examen

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,7 @@
 import "./Home.css"
 
+import basedatos from "../utils/basedatos.json"
+
 export function Home(){
 
     //logica para leer la base de datos
@@ -32,9 +34,24 @@ export function Home(){
 
        
         <section className="container">
-            <section className="row-cols-1 row-cols-md-5 g-3">
+            <section className="row row-cols-12 row-cols-md-5 g-3">
                 {
                     //logica para recorrer la BD y mostrar la informacion de los 10 barberos
+                    basedatos.map(function(barbero){
+                        return(
+                            <div className="col">
+                                <div className="card h-100 shadow px-2">    
+                                    <h3>{barbero.nombre}</h3>
+                                    <h4>{barbero.especialidad}</h4>
+                                    <h4>Experiencia: {barbero.experiencia}</h4>
+                                    <h4>{barbero.correo}</h4>
+                                    <h4>{barbero.contacto}</h4>
+                                </div>
+                            </div>
+                        )
+                    })
+
+
                 }
             </section>
         </section>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//Se eliminan los dos import que no están relacionados
+//import App from './App.jsx'
+//import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 import { Home } from './Home/Home.jsx'
 
@@ -10,6 +14,9 @@ import { Home } from './Home/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Home>
+
+    </Home>
     
   </React.StrictMode>,
 )
